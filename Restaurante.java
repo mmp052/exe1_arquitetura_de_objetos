b9939ac091476909c99162a19bc3306dd5702f1d
MMp <mateusmoreirammp052@gmail.com>
2024-02-15 17:14:37 -0300

    main

---
+++ Restaurante.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class Restaurante {
+    public String nome;
+    public double avaliacao;
+    public ArrayList<Item> cardapio;
+
+    public Restaurante(String nome,double avaliacao,ArrayList<Item> cardapio){
+        this.nome = nome;
+        this.avaliacao = avaliacao;
+        this.cardapio = cardapio;
+    }
+
+    public void get_nome(){
+        System.out.println("nome" + this.nome);
+    }
+
+    public void adicionar_item(){
+        for(Item item : this.cardapio){
+            System.out.println(item.nome + " : " + item.preco);
+        }
+    }
+}
