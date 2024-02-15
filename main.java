47a817aa3cb36f84ad121e5428700d224c3d4361
MMp <mateusmoreirammp052@gmail.com>
2024-02-15 17:29:16 -0300

    fazendo o main

--- main.java
+++ main.java
@@ -1,6 +1,23 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
     
-        
+        ArrayList<Item> items = new ArrayList<Item>();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true){
+            System.out.println("Menu principal");
+            System.out.println("1. Cadastrar Restaurante");
+            System.out.println("2. Listar Restaurantes");
+            System.out.println("3. Cadastrar item no menu de um restaurante");
+            System.out.println("4. Sair");
+
+            System.out.println("Escolha uma opção");
+            String opcao = scanner.nextLine();
+
+            
+        }
     }
 }
