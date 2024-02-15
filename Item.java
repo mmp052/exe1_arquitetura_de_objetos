2abaabc27e94e117550caae3f5073539b035aec6
Daniel Prado <daniel.vascomadagama2@gmail.com>
2024-02-15 17:09:35 -0300

    classe item feita

---
+++ Item.java
@@ -0,0 +1,21 @@
+public class Item {
+
+    public String nome;
+    public double preco;
+
+    public Item(String nome, double preco) {
+
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public void getNome(String nome) {
+        System.out.println("Nome" + nome);
+    }
+
+}
+
+
+
+
+
